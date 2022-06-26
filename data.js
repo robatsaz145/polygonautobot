0ad6268342604ec32c58edecbd92566515e40a1d
robatsaz145 <89588786+robatsaz145@users.noreply.github.com>
2022-06-26 21:29:30 +0430

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'',//database link
+    
+    bot_token:'',//bot token
+    
+    bot_name:'PolygonCustomAutoPaybyHyperbot',//bot username without @
+    
+    bot_admin:1915849115,// admin telegram id
+    
+    channelsList : ['@'],// channels list
+    
+    reffer_bonus:5,// refferal bonus amount
+    
+    min_wd:1,//min withdrawal amount
+    
+    daily_bonus:100,// daily bonus amount
+    
+    currency:'', //bot currency
+    
+    contractaddress :  '', //contract address of coin
+    
+    privateKey : '', //private key of wallet
+    
+    address : '',//polygon address of wallet
+    
+    payment_channel:'@'// payment channel username
+    
+    }
