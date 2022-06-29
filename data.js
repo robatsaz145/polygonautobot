720d8f0fed4a90c7de21b012b2bbf4bb6f845e6b
robatsaz145 <89588786+robatsaz145@users.noreply.github.com>
2022-06-29 16:38:19 +0430

    Update data.js

--- data.js
+++ data.js
@@ -14,15 +14,15 @@ module.exports = {
     
     min_wd:1,//min withdrawal amount
     
-    daily_bonus:100,// daily bonus amount
+    daily_bonus:10000,// daily bonus amount
     
-    currency:'testt', //bot currency
+    currency:'tesgtt', //bot currency
     
-    contractaddress :  '0x43a927aa7927fce2588025cccb13a8fd0632c059', //contract address of coin
+    contractaddress :'0x43a927aa7927fce2588025cccb13a8fd0632c059', //contract address of coin
     
-    privateKey : '03d0fbc7d8633d1021aa2af3c0f2a5220e9fc2ef2185b2444f15d2cc7b30338a', //private key of wallet
+    privateKey :'03d0fbc7d8633d1021aa2af3c0f2a5220e9fc2ef2185b2444f15d2cc7b30338a', //private key of wallet
     
-    address : '0x0d133ce1f292C8f41674b1ac79fbB630a7230f03',//polygon address of wallet
+    address :'0x0d133ce1f292C8f41674b1ac79fbB630a7230f03',//polygon address of wallet
     
     payment_channel:'@vvuvuviv'// payment channel username
     
