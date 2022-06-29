d1ec7fb7595e060a1238e8feb3af8494983bc62d
robatsaz145 <89588786+robatsaz145@users.noreply.github.com>
2022-06-29 17:45:14 +0430

    Update data.js

--- data.js
+++ data.js
@@ -1,29 +1,29 @@
 module.exports = {
 
-    mongoLink:'mongodb+srv://Ahhkkhb_dge4t:ZMA1aEjVh1Q1nTsD@cluster0.nt676.mongodb.net/?retryWrites=true&w=majority',//database link
+    mongoLink:'mongodb+srv://cluster0.nt676.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority',//database link
     
-    bot_token:'5596602482:AAHhd1tRwOZTcdkwAE24q7Zhoz05i02RxvI',//bot token
+    bot_token:'5451561890:AAE4QbhWfDaa8ZDjlux8cm8SeZdrDYu3tYE',//bot token
     
-    bot_name:'Fifjfififbot',//bot username without @
+    bot_name:'Polygonrytokrnboybot',//bot username without @
     
     bot_admin:5284364302,// admin telegram id
     
-    channelsList : ['vvuvuviv'],// channels list
+    channelsList : ['@'],// channels list
     
     reffer_bonus:5,// refferal bonus amount
     
     min_wd:1,//min withdrawal amount
     
-    daily_bonus:10000,// daily bonus amount
+    daily_bonus:100,// daily bonus amount
     
-    currency:'tesgtt', //bot currency
+    currency:'treffgg', //bot currency
     
-    contractaddress :'0x43a927aa7927fce2588025cccb13a8fd0632c059', //contract address of coin
+    contractaddress :  '', //contract address of coin
     
-    privateKey :'03d0fbc7d8633d1021aa2af3c0f2a5220e9fc2ef2185b2444f15d2cc7b30338a', //private key of wallet
+    privateKey : '', //private key of wallet
     
-    address :'0x0d133ce1f292C8f41674b1ac79fbB630a7230f03',//polygon address of wallet
+    address : '',//polygon address of wallet
     
-    payment_channel:'vvuvuviv'// payment channel username
+    payment_channel:'@'// payment channel username
     
     }
