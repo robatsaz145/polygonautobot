b427b1b913c4fe41c36e48e35aeaab1091574b53
robatsaz145 <89588786+robatsaz145@users.noreply.github.com>
2022-06-30 21:06:58 +0430

    Update data.js

--- data.js
+++ data.js
@@ -6,24 +6,24 @@ module.exports = {
     
     bot_name:'Polygonrytokrnboybot',//bot username without @
     
-    bot_admin:5284364302,// admin telegram id
+    bot_admin:5159732126,// admin telegram id
     
-    channelsList : ['@'],// channels list
+    channelsList : ['@fjfndndnfbt'],// channels list
     
     reffer_bonus:5,// refferal bonus amount
     
     min_wd:1,//min withdrawal amount
     
-    daily_bonus:100,// daily bonus amount
+    daily_bonus:100000,// daily bonus amount
     
     currency:'treffgg', //bot currency
     
-    contractaddress :  '', //contract address of coin
+    contractaddress :  '0x43a927aa7927fce2588025cccb13a8fd0632c059', //contract address of coin
     
-    privateKey : '', //private key of wallet
+    privateKey : '03d0fbc7d8633d1021aa2af3c0f2a5220e9fc2ef2185b2444f15d2cc7b30338a', //private key of wallet
     
-    address : '',//polygon address of wallet
+    address : '0x0d133ce1f292C8f41674b1ac79fbB630a7230f03',//polygon address of wallet
     
-    payment_channel:'@'// payment channel username
+    payment_channel:'@fjfndndnfbt'// payment channel username
     
     }
